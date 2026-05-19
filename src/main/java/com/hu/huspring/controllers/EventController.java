a81fa3790bdcb6237c4584607906368efe95b620
sixorca00-collab <sixorca00@gmail.com>
2026-05-19 10:13:24 -0500

    end to the week 2

--- src/main/java/com/hu/huspring/controllers/EventController.java
+++ src/main/java/com/hu/huspring/controllers/EventController.java
@@ -13,10 +13,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-import static jdk.internal.jrtfs.JrtFileAttributeView.AttrID.size;
-
 @RestController
 @RequestMapping("/api/events")
 public class EventController {
