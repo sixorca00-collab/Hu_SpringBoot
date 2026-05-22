80e6ded314218d56850dd894e342ac8b2c68ac6f
SAOT31 <saot-31@hotmail.com>
2026-05-22 06:46:23 -0500

    fix(Event): when made a merge git delete the relation in this file

--- src/main/java/com/hu/huspring/models/Event.java
+++ src/main/java/com/hu/huspring/models/Event.java
@@ -5,26 +5,32 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 @Table(name = "Events")
 @Entity
 public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Enumerated(EnumType.STRING)
     private EventType type;
+
     @Temporal(TemporalType.TIMESTAMP)
     private Date startDate;
+
     @Temporal(TemporalType.TIMESTAMP)
     private Date endDate;
+
     @Column(name = "description")
     String desc;
-}
 
+    //Esto lo elimino el git y en estas notaciones vive la relacion
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "venue_id", nullable = true)
+    private Venue venue;
+}
