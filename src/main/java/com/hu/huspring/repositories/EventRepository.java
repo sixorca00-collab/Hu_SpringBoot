6a5a903c5326dccc76a6baa557aed783e5cc057b
Sixorca00-collab <sixorca00@gmail.com>
2026-05-29 07:04:21 -0500

    feat(EventRepository): add nested Query to design the response

--- src/main/java/com/hu/huspring/repositories/EventRepository.java
+++ src/main/java/com/hu/huspring/repositories/EventRepository.java
@@ -1,7 +1,11 @@
 package com.hu.huspring.repositories;
 
+import com.hu.huspring.dtos.EventDTO;
 import com.hu.huspring.models.Event;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +14,6 @@ import java.util.List;
 public interface EventRepository extends JpaRepository<Event, Long> {
 
     List<Event> findByDescContaining(String text);
+    @Query("SELECT new com.hu.huspring.dto.EventDTO(e.id, e.n, e.startDate, v.name, v.city) FROM Event e JOIN e.venue v")
+    Slice<EventDTO> findByFilters(Pageable pageable);
 }
