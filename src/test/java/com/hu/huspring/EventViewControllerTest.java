85d630cc8752590231ef4c7524ab058623db1196
Sixorca00-collab <sixorca00@gmail.com>
2026-05-29 08:42:15 -0500

    test(view): update mvc coverage for admin event screen

--- src/test/java/com/hu/huspring/EventViewControllerTest.java
+++ src/test/java/com/hu/huspring/EventViewControllerTest.java
@@ -1,14 +1,20 @@
 package com.hu.huspring;
 
 import com.hu.huspring.controllers.view.EventViewController;
+import com.hu.huspring.dtos.EventDTO;
 import com.hu.huspring.services.EventService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -26,10 +32,13 @@ public class EventViewControllerTest {
 
     @Test
     public void testListEventsViewShouldReturnSuccessAndModel() throws Exception {
+        Slice<EventDTO> events = new SliceImpl<>(List.of());
+        when(eventService.getAllPaginated(org.springframework.data.domain.Pageable.unpaged())).thenReturn(events);
+
         mockMvc.perform(get("/admin/events")
                         .contentType(MediaType.TEXT_HTML))
                 .andExpect(status().isOk()) // Valida estado 200 OK
-                .andExpect(view().name("event-list")) // Valida que busque 'event-list.html'
+                .andExpect(view().name("events-list")) // Valida que busque 'events-list.html'
                 .andExpect(model().attributeExists("events")); // Valida que el objeto 'events' vaya en el modelo
     }
 }
